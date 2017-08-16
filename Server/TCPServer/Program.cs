d1e61a5d0726890162242ec4885c1b012b466dbe
RadialApps <radialapps@gmail.com>
2017-08-16 22:46:12 +0530

    Add threading for back-to-back receiving

--- Server/TCPServer/Program.cs
+++ Server/TCPServer/Program.cs
@@ -12,9 +12,6 @@ namespace TCPServer
             /* Port for incoming connections */
             const int PORT = 3800;
 
-            /* Create a buffer for receiving */
-            byte[] receiveBytes = new byte[1024];
-
             /* The IPEndPoint for the server. IP cannot be localhost */
             System.Net.IPEndPoint remoteIpEndPoint = new System.Net.IPEndPoint(System.Net.IPAddress.Any, PORT);
             Console.WriteLine("Listening for connections on port " + PORT.ToString());
@@ -27,91 +24,107 @@ namespace TCPServer
             System.Net.Sockets.TcpListener tcpListener = new System.Net.Sockets.TcpListener(remoteIpEndPoint);
             tcpListener.Start();
 
-            /* The socket that will be used for listening */
-            System.Net.Sockets.Socket sock = null;
+            /* Create the listening thread */
+            Console.WriteLine("Creating the Child Thread");
+            System.Threading.Thread connectThread = new System.Threading.Thread(() => StartConnect(tcpListener, TIMEOUT, FILE_NAME));
+            connectThread.Start();
 
-            /* FileStream for writing */
-            System.IO.FileStream objWriter = null;
-
-            /* Number and total number of bytes read till the end of loop */
-            int bytesRead = 0;
-            int totalBytesRead = 0;            
-
-            /* Loop till something is read */
-            while (totalBytesRead == 0) {
-
-                /* Sleep for 100ms if no connection is being made */
-                while (!tcpListener.Pending()) System.Threading.Thread.Sleep(100);
-
-                sock = tcpListener.AcceptSocket();
-                Console.WriteLine("Accepted Connection");
-                sock.ReceiveTimeout = TIMEOUT;
-
-                /* Sleep for another 100ms to give the client time to respond */
-                System.Threading.Thread.Sleep(100);
-                int filesize = 0;
-                try
-                {
-                    /* Receive the header, terminate if not found */
-                    if ((bytesRead = sock.Receive(receiveBytes)) > 0)
-                    {
-                        string[] headers = System.Text.Encoding.ASCII.GetString(receiveBytes).Split('\n');
-                        if (headers[0] == "HEADER")
-                        {
-                            Console.WriteLine("Receiving file of size " + headers[1] + " bytes");
-                            Int32.TryParse(headers[1], out filesize);
-                        }
-                        else throw new Exception("No header received");
-                    }
-                    else throw new Exception("No header received");
-
-                    while ((totalBytesRead != filesize) && (bytesRead = sock.Receive(receiveBytes,receiveBytes.Length, System.Net.Sockets.SocketFlags.None )) > 0)
-                    {
-                        /* Delete existing file to be safe */
-                        if (objWriter == null)
-                        {
-                            if (System.IO.File.Exists(FILE_NAME)) System.IO.File.Delete(FILE_NAME);
-                            objWriter = System.IO.File.OpenWrite(FILE_NAME);
-                        }
-
-                        objWriter.Write(receiveBytes, 0, bytesRead);
-
-                        totalBytesRead += bytesRead;
-
-                        /* Reduce buffer size if bytes left are less than it */
-                        if(filesize - totalBytesRead < receiveBytes.Length)
-                        {
-                            receiveBytes = new byte[filesize - totalBytesRead];
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-
-                /* Close everything */
-                sock.Close();
-                if (!(objWriter == null))
-                {
-                    objWriter.Close();
-                    objWriter = null;
-                }
-                Console.WriteLine("Closed Connection");
-            }
+            /* Terminate on keypress */
+            Console.ReadKey();
+            connectThread.Abort();
 
             /* Clean up and open the received file */
             tcpListener.Stop();
             System.Diagnostics.Process.Start(FILE_NAME);
-            Console.ReadKey();
         }
 
-        static string GetLocalIPAddress()
+        public static void StartConnect(System.Net.Sockets.TcpListener tcpListener, int TIMEOUT, String FILE_NAME)
         {
-            foreach (System.Net.IPAddress ip in System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName()).AddressList)
-                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-                    return ip.ToString();
-            throw new Exception("Local IP Address Not Found!");
+            try
+            {
+                while (true)
+                {
+                    /* Create a buffer for receiving */
+                    byte[] receiveBytes = new byte[1024];
+
+                    /* The socket that will be used for listening */
+                    System.Net.Sockets.Socket sock = null;
+
+                    /* FileStream for writing */
+                    System.IO.FileStream objWriter = null;
+
+                    /* Number and total number of bytes read till the end of loop */
+                    int bytesRead = 0;
+                    int totalBytesRead = 0;
+
+                    /* Loop till something is read */
+                    while (totalBytesRead == 0)
+                    {
+                        /* Sleep for 100ms if no connection is being made */
+                        while (!tcpListener.Pending()) System.Threading.Thread.Sleep(100);
+
+                        sock = tcpListener.AcceptSocket();
+                        Console.WriteLine("Accepted Connection");
+                        sock.ReceiveTimeout = TIMEOUT;
+
+                        /* Sleep for another 100ms to give the client time to respond */
+                        System.Threading.Thread.Sleep(100);
+                        int filesize = 0;
+                        try
+                        {
+                            /* Receive the header, terminate if not found */
+                            if ((bytesRead = sock.Receive(receiveBytes)) > 0)
+                            {
+                                string[] headers = System.Text.Encoding.ASCII.GetString(receiveBytes).Split('\n');
+                                if (headers[0] == "HEADER")
+                                {
+                                    Console.WriteLine("Receiving file of size " + headers[1] + " bytes");
+                                    Int32.TryParse(headers[1], out filesize);
+                                }
+                                else throw new Exception("No header received");
+                            }
+                            else throw new Exception("No header received");
+
+                            while ((totalBytesRead != filesize) && (bytesRead = sock.Receive(receiveBytes, receiveBytes.Length, System.Net.Sockets.SocketFlags.None)) > 0)
+                            {
+                                /* Delete existing file to be safe */
+                                if (objWriter == null)
+                                {
+                                    if (System.IO.File.Exists(FILE_NAME)) System.IO.File.Delete(FILE_NAME);
+                                    objWriter = System.IO.File.OpenWrite(FILE_NAME);
+                                }
+
+                                objWriter.Write(receiveBytes, 0, bytesRead);
+
+                                totalBytesRead += bytesRead;
+
+                                /* Reduce buffer size if bytes left are less than it */
+                                if (filesize - totalBytesRead < receiveBytes.Length)
+                                {
+                                    receiveBytes = new byte[filesize - totalBytesRead];
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+                        /* Close everything */
+                        sock.Close();
+                        if (!(objWriter == null))
+                        {
+                            objWriter.Close();
+                            objWriter = null;
+                        }
+                        Console.WriteLine("Closed Connection");
+                    }
+                }
+            }
+            catch (System.Threading.ThreadAbortException)
+            {
+                Console.WriteLine("Thread Abort");
+            }
         }
     }
 }
--- Server/TCPServerVB/Program.vb
+++ Server/TCPServerVB/Program.vb
@@ -7,12 +7,9 @@
             ' Port for incoming connections
             Const PORT As Integer = 3800
 
-            ' Create a buffer for receiving 
-            Dim receiveBytes As Byte() = New Byte(1023) {}
-
             ' The IPEndPoint for the server. IP cannot be localhost 
             Dim remoteIpEndPoint As New System.Net.IPEndPoint(System.Net.IPAddress.Any, PORT)
-            Console.WriteLine("Listening for connections on port " + CStr(PORT))
+            Console.WriteLine("Listening for connections on port " + PORT.ToString())
 
             ' After this amount of time has passed, any connection will be terminated
             ' Keep high for high latency networks and vice versa 
@@ -22,90 +19,101 @@
             Dim tcpListener As New System.Net.Sockets.TcpListener(remoteIpEndPoint)
             tcpListener.Start()
 
-            ' The socket that will be used for listening 
-            Dim sock As System.Net.Sockets.Socket = Nothing
+            'Create the listening thread
+            Console.WriteLine("Creating the Child Thread")
+            Dim connectThread As New System.Threading.Thread(Sub() StartConnect(tcpListener, TIMEOUT, FILE_NAME))
+            connectThread.Start()
 
-            ' FileStream for writing 
-            Dim objWriter As IO.FileStream = Nothing
-
-            ' Number and total number of bytes read till the end of loop 
-            Dim bytesRead As Integer = 0
-            Dim totalBytesRead As Integer = 0
-
-            ' Loop till something is read 
-            While totalBytesRead = 0
-
-                ' Sleep for 100ms if no connection is being made 
-                While Not tcpListener.Pending()
-                    System.Threading.Thread.Sleep(100)
-                End While
-
-                sock = tcpListener.AcceptSocket()
-                Console.WriteLine("Accepted Connection")
-                sock.ReceiveTimeout = TIMEOUT
-
-                ' Sleep for another 100ms to give the client time to respond 
-                System.Threading.Thread.Sleep(100)
-                Dim filesize As Integer = 0
-                Try
-                    ' Receive the header, terminate if not found 
-                    If (Assign(bytesRead, sock.Receive(receiveBytes))) > 0 Then
-                        Dim headers As String() = System.Text.Encoding.ASCII.GetString(receiveBytes).Split(ControlChars.Lf)
-                        If headers(0) = "HEADER" Then
-                            Console.WriteLine("Receiving file of size " + headers(1) + " bytes")
-                            Int32.TryParse(headers(1), filesize)
-                        Else
-                            Throw New Exception("No header received")
-                        End If
-                    Else
-                        Throw New Exception("No header received")
-                    End If
-
-                    While (totalBytesRead <> filesize) AndAlso (Assign(bytesRead, sock.Receive(receiveBytes, receiveBytes.Length, System.Net.Sockets.SocketFlags.None))) > 0
-                        ' Delete existing file to be safe 
-                        If objWriter Is Nothing Then
-                            If System.IO.File.Exists(FILE_NAME) Then
-                                System.IO.File.Delete(FILE_NAME)
-                            End If
-                            objWriter = System.IO.File.OpenWrite(FILE_NAME)
-                        End If
-
-                        objWriter.Write(receiveBytes, 0, bytesRead)
-
-                        totalBytesRead += bytesRead
-
-                        ' Reduce buffer size if bytes left are less than it
-                        If filesize - totalBytesRead < receiveBytes.Length Then
-                            receiveBytes = New Byte(filesize - totalBytesRead - 1) {}
-                        End If
-                    End While
-                Catch e As Exception
-                    Console.WriteLine(e.Message)
-                End Try
-
-                ' Close everything
-                sock.Close()
-                If Not IsNothing(objWriter) Then
-                    objWriter.Close()
-                    objWriter = Nothing
-                End If
-                Console.WriteLine("Closed Connection")
-            End While
+            'Terminate on keypress
+            Console.ReadKey()
+            connectThread.Abort()
 
             ' Clean up and open the received file 
             tcpListener.Stop()
             System.Diagnostics.Process.Start(FILE_NAME)
-            Console.ReadKey()
         End Sub
 
-        Private Shared Function GetLocalIPAddress() As String
-            For Each ip As Net.IPAddress In System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName()).AddressList
-                If (ip.AddressFamily = System.Net.Sockets.AddressFamily.InterNetwork) Then Return ip.ToString()
-            Next
-            Throw New Exception("Local IP Address Not Found!")
-        End Function
+        Public Shared Sub StartConnect(tcpListener As System.Net.Sockets.TcpListener, TIMEOUT As Integer, FILE_NAME As [String])
+            Try
+                While True
+                    ' Create a buffer for receiving
+                    Dim receiveBytes As Byte() = New Byte(1023) {}
 
-        Private Shared Function Assign(Of T)(ByRef target As T, value As T) As T
+                    ' The socket that will be used for listening
+                    Dim sock As System.Net.Sockets.Socket = Nothing
+
+                    ' FileStream for writing
+                    Dim objWriter As System.IO.FileStream = Nothing
+
+                    ' Number and total number of bytes read till the end of loop
+                    Dim bytesRead As Integer = 0
+                    Dim totalBytesRead As Integer = 0
+
+                    ' Loop till something is read
+                    While totalBytesRead = 0
+
+                        ' Sleep for 100ms if no connection is being made
+                        While Not tcpListener.Pending()
+                            System.Threading.Thread.Sleep(100)
+                        End While
+
+                        sock = tcpListener.AcceptSocket()
+                        Console.WriteLine("Accepted Connection")
+                        sock.ReceiveTimeout = TIMEOUT
+
+                        ' Sleep for another 100ms to give the client time to respond
+                        System.Threading.Thread.Sleep(100)
+                        Dim filesize As Integer = 0
+                        Try
+                            ' Receive the header, terminate if not found
+                            If (InlineAssignHelper(bytesRead, sock.Receive(receiveBytes))) > 0 Then
+                                Dim headers As String() = System.Text.Encoding.ASCII.GetString(receiveBytes).Split(ControlChars.Lf)
+                                If headers(0) = "HEADER" Then
+                                    Console.WriteLine("Receiving file of size " + headers(1) + " bytes")
+                                    Int32.TryParse(headers(1), filesize)
+                                Else
+                                    Throw New Exception("No header received")
+                                End If
+                            Else
+                                Throw New Exception("No header received")
+                            End If
+
+                            While (totalBytesRead <> filesize) AndAlso (InlineAssignHelper(bytesRead, sock.Receive(receiveBytes, receiveBytes.Length, System.Net.Sockets.SocketFlags.None))) > 0
+                                ' Delete existing file to be safe
+                                If objWriter Is Nothing Then
+                                    If System.IO.File.Exists(FILE_NAME) Then
+                                        System.IO.File.Delete(FILE_NAME)
+                                    End If
+                                    objWriter = System.IO.File.OpenWrite(FILE_NAME)
+                                End If
+
+                                objWriter.Write(receiveBytes, 0, bytesRead)
+
+                                totalBytesRead += bytesRead
+
+                                ' Reduce buffer size if bytes left are less than it
+                                If filesize - totalBytesRead < receiveBytes.Length Then
+                                    receiveBytes = New Byte(filesize - totalBytesRead - 1) {}
+                                End If
+                            End While
+                        Catch e As Exception
+                            Console.WriteLine(e.Message)
+                        End Try
+
+                        ' Close everything
+                        sock.Close()
+                        If Not (objWriter Is Nothing) Then
+                            objWriter.Close()
+                            objWriter = Nothing
+                        End If
+                        Console.WriteLine("Closed Connection")
+                    End While
+                End While
+            Catch e As System.Threading.ThreadAbortException
+                Console.WriteLine("Thread Abort")
+            End Try
+        End Sub
+        Private Shared Function InlineAssignHelper(Of T)(ByRef target As T, value As T) As T
             target = value
             Return value
         End Function
